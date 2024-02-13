7f06d206908993efc99fe98249f6651f2f0e54e9
sela123 <shalev2dahan@gmail.com>
2024-02-13 15:27:35 +0200

    fixed fourth answer again

--- basics/4-Celsius-to-Fahrenheit.py
+++ basics/4-Celsius-to-Fahrenheit.py
@@ -2,7 +2,7 @@ kind = input("what you want to convert, C or F: ")
 num = int(input("enter the temperature: "))
 if kind == "C":
     print("the temperature in fahrenheit is: ", ((num * 1.8) + 32))
-elif kind == "f":
+elif kind == "F":
     print("the temperature in celsius is: ", ((num - 32) / 1.8))
 else:
     print("invalid choice")
