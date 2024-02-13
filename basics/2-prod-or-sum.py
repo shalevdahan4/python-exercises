f17c8d7ab1ecbde7a5b48851bc2451f5bd18a385
sela123 <shalev2dahan@gmail.com>
2024-02-13 14:25:13 +0200

    added second answer

---
+++ basics/2-prod-or-sum.py
@@ -0,0 +1,6 @@
+n1 = int(input("enter number: "))
+n2 = int(input("enter second number: "))
+if (n1 * n2) < 1000:
+    print("the product is: ", (n1 * n2))
+else:
+    print("the sum of the two numbers is: ", (n1 + n2))
