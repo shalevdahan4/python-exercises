e4407212030d8b4d853197e869f1169c910a95a5
sela123 <shalev2dahan@gmail.com>
2024-02-13 15:00:41 +0200

    fixed forth answer

--- basics/4-Celsius-to-Fahrenheit.py
+++ basics/4-Celsius-to-Fahrenheit.py
@@ -1,2 +1,6 @@
-num = int(input("enter the temperature in celsius: "))
-print("the temperature in fahrenheit is: ", ((num * 1.8) + 32))
+kind = input("what you want to convert, celsius or fahrenheit: ")
+num = int(input("enter the temperature: "))
+if kind == "celsius":
+    print("the temperature in fahrenheit is: ", ((num * 1.8) + 32))
+else:
+    print("the temperature in celsius is: ", ((num - 32) / 1.8))
